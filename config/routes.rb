ee73af221e327179a3d3524f473074f5479cb40e
Pavan Thammanna <8501597+Pavanct@users.noreply.github.com>
2024-04-04 14:27:55 +0200

    Refactor routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
-  get 'animes/index'
-  get 'animes/new'
-  get 'animes/create'
-  get 'animes/edit'
-  get 'animes/update'
-  get 'animes/destroy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :animes
+
+  resources :users
 end
