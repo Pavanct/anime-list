6a992506fb7a6d52df5ed5e685fbc22bc0397e99
Pavan Thammanna <8501597+Pavanct@users.noreply.github.com>
2024-04-22 23:43:08 +0200

    Add show action to AnimeListsController and update routes.rb

--- app/controllers/anime_lists_controller.rb
+++ app/controllers/anime_lists_controller.rb
@@ -6,6 +6,11 @@ class AnimeListsController < ApplicationController
     render json: @anime_lists
   end
 
+  def show
+    @anime_list = AnimeList.find(params[:id])
+    render json: @anime_list
+  end
+
   def create
     @anime_list = AnimeList.new(anime_list_params)
     @anime_list.user_id = params[:user_id]
@@ -22,6 +27,26 @@ class AnimeListsController < ApplicationController
   end
 
   def update
+    @anime_list = AnimeList.find(params[:id])
+    if @anime_list.update(anime_list_params)
+      if params[:anime_ids].present?
+        current_anime_ids = @anime_list.animes.pluck(:id)
+        new_anime_ids = params[:anime_ids].map(&:to_i)
+
+        animes_to_add = new_anime_ids - current_anime_ids
+        animes_to_remove = current_anime_ids - new_anime_ids
+
+        animes_to_add.each do |anime_id|
+          UserAnime.create(anime_list: @anime_list, anime_id: anime_id, user_id: params[:user_id])
+        end
+
+        @anime_list.user_animes.where(anime_id: animes_to_remove).destroy_all
+      end
+
+      render json: @anime_list
+    else
+      render json: @anime_list.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :animes
 
   resources :users do
-    resources :anime_lists, only: [:index, :create]
+    resources :anime_lists, only: [:index, :show, :create, :update, :destroy]
   end
 end
