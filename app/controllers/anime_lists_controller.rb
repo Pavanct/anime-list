0d24922f0753d7c76c5422d68756001443b5a410
Pavan Thammanna <8501597+Pavanct@users.noreply.github.com>
2024-04-23 19:22:33 +0200

    Add update_episodes_watched method

--- app/controllers/anime_lists_controller.rb
+++ app/controllers/anime_lists_controller.rb
@@ -1,6 +1,6 @@
 class AnimeListsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :update, :destroy]
-  before_action :set_anime_list, only: [:show, :update, :destroy]
+  before_action :authenticate_user!, only: [:create, :update, :destroy, :update_episodes_watched]
+  before_action :set_anime_list, only: [:show, :update, :destroy, :update_episodes_watched]
   before_action :authorize_user!, only: [:update, :destroy]
   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format =~ /^application\/json/ }
 
@@ -15,7 +15,7 @@ class AnimeListsController < ApplicationController
   end
 
   def create
-    @anime_list = AnimeList.new(anime_list_params)
+    @anime_list = AnimeList.new(create_params)
     @anime_list.user_id = params[:user_id]
 
     if @anime_list.save
@@ -31,7 +31,7 @@ class AnimeListsController < ApplicationController
 
   def update
     @anime_list = AnimeList.find(params[:id])
-    if @anime_list.update(anime_list_params)
+    if @anime_list.update(update_params)
       if params[:anime_ids].present?
         current_anime_ids = @anime_list.animes.pluck(:id)
         new_anime_ids = params[:anime_ids].map(&:to_i)
@@ -63,6 +63,17 @@ class AnimeListsController < ApplicationController
     end
   end
 
+  def update_episodes_watched
+    user_anime = @anime_list.user_animes.find_by(anime_id: update_episodes_watched_params[:anime_id])
+
+    if user_anime
+      user_anime.update(episodes_watched: update_episodes_watched_params[:episodes_watched])
+      render json: user_anime
+    else
+      render json: { error: 'User anime not found.' }, status: :not_found
+    end
+  end
+
   private
 
   def set_anime_list
@@ -75,7 +86,15 @@ class AnimeListsController < ApplicationController
     end
   end
 
-  def anime_list_params
+  def create_params
+    params.require(:anime_list).permit(:name).merge(user_id: params[:user_id])
+  end
+
+  def update_params
     params.require(:anime_list).permit(:name)
+  end
+
+  def update_episodes_watched_params
+    params.permit(:anime_id, :episodes_watched)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   root to: 'animes#index'
 
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   resources :animes
 
-  resources :users do
-    resources :anime_lists, only: [:index, :show, :create, :update, :destroy]
+  resources :users, only: [:index]  do
+    resources :anime_lists, only: [:index, :show, :create, :update, :destroy] do
+      member do
+        put :update_episodes_watched
+      end
+    end
   end
 end
