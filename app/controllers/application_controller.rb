3010e465fd4b083de573cae08d2f67989020f99c
Pavan Thammanna <8501597+Pavanct@users.noreply.github.com>
2024-04-23 07:59:14 +0200

    Add devise authentication for sign up with name parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format =~ /^application\/json/ }
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'animes#index'
+
   devise_for :users
   resources :animes
 
